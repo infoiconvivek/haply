419e4ccc78bab71e86977528ceb7d07f6c870855
vivekumar <vivekbcagcg@gmail.com>
2024-02-02 10:52:47 +0530

    new update

---
+++ wp-content/uploads/cache/wpml/twig/9a/9ac6eeb939d4898d6f87677ced1016c3f4d3cf7b87046606018eed4d276dd55e.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace WPML\Core;
+
+use \WPML\Core\Twig\Environment;
+use \WPML\Core\Twig\Error\LoaderError;
+use \WPML\Core\Twig\Error\RuntimeError;
+use \WPML\Core\Twig\Markup;
+use \WPML\Core\Twig\Sandbox\SecurityError;
+use \WPML\Core\Twig\Sandbox\SecurityNotAllowedTagError;
+use \WPML\Core\Twig\Sandbox\SecurityNotAllowedFilterError;
+use \WPML\Core\Twig\Sandbox\SecurityNotAllowedFunctionError;
+use \WPML\Core\Twig\Source;
+use \WPML\Core\Twig\Template;
+
+/* / */
+class __TwigTemplate_5cd110cbba0becd527d313c32a97c1ea98233f0e189a7f7ef869ae38f1c0d58a extends \WPML\Core\Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "/";
+    }
+
+    public function getTemplateName()
+    {
+        return "/";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  32 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "/", "");
+    }
+}
